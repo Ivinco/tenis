a7b8c3cf9c66e7dc50ed9dd1cc0d64ac9a82842e
bg502 <andrew.kozlowskiy@gmail.com>
2023-11-10 19:43:19 +0400

    fixed

--- backend/src/tenis_backend/__main__.py
+++ backend/src/tenis_backend/__main__.py
@@ -1,6 +1,6 @@
 from os import getenv
 from .app import app
 
-port = getenv('TENIS_BACKEND_PORT', '8000')
+port = getenv('LISTEN_PORT', '8000')
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(port))
