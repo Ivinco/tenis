716b842553cefe54e4607c96902af11fb244b1cc
vaniacer <ie.marov@gmail.com>
2023-11-13 22:57:44 +0300

    MNU-120: adding websocket support via flask-sock

--- backend/src/tenis_backend/app.py
+++ backend/src/tenis_backend/app.py
@@ -1,7 +1,9 @@
-import os, jwt
+import os
+import jwt
+import atexit
 from flask import Flask, request
 from pymongo import MongoClient
-import atexit
+from flask_sock import Sock
 from .auth import token_required
 from .user import User
 
@@ -9,28 +11,62 @@ from .user import User
 mongo_host = os.getenv('MONGO_HOST', 'localhost')  # Default to 'localhost' if not set
 mongo_dbname = os.getenv('MONGO_DBNAME', 'tenis')  # Default to 'database' if not set
 connection_string = f"mongodb://{mongo_host}/{mongo_dbname}"
-secret_key = os.getenv('SECRET', 'big tenis') # Secret key to use 
+secret_key = os.getenv('SECRET', 'big tenis')  # Secret key to use
 
 app = Flask(__name__)
+sock = Sock(app)
 app.mongodb_client = MongoClient(connection_string)
 app.db = app.mongodb_client[mongo_dbname]
 app.config['SECRET_KEY'] = secret_key
+
 
 def cleanup_on_shutdown():
     """Properly close MongoDB connection on shutdown"""
     app.mongodb_client.close()
 
+
 atexit.register(cleanup_on_shutdown)
+
+
+@sock.route('/alerts')
+def alerts(sock):
+    while True:
+        data = sock.receive()
+        sock.send(data)
+
+
+@sock.route('/new')
+def new(sock):
+    while True:
+        data = sock.receive()
+        sock.send(data)
+
+
+@sock.route('/changed')
+def changed(sock):
+    while True:
+        data = sock.receive()
+        sock.send(data)
+
+
+@sock.route('/resolved')
+def resolved(sock):
+    while True:
+        data = sock.receive()
+        sock.send(data)
+
 
 @app.route("/")
 def index():
     return "Hello, world!\n", 200
+
 
 @app.route("/whoami")
 @token_required
 def whoami(current_user):
     """Sample method to demonstrate @token_required decorator"""
     return "You are authorized as %s\n" % (current_user['email']), 200
+
 
 @app.route("/healz")
 def healz():
@@ -40,12 +76,13 @@ def healz():
         return f"Mongo error: {e!r}\n", 500
     return "tenis is big as ever\n", 200
 
+
 @app.route("/auth", methods=["POST"])
 def login():
     try:
-        data = request.get_json(force = True, silent = False)
+        data = request.get_json(force=True, silent=False)
         if not data:
-            return dict(message = "Please provide user credentials in JSON format", data = None, error = "Bad request"), 400
+            return dict(message="Please provide user credentials in JSON format", data=None, error="Bad request"), 400
 
         user = User().login(
             data.get('email'),
@@ -54,16 +91,16 @@ def login():
         if user:
             try:
                 user["token"] = jwt.encode(
-                    { "user_id": user["_id"] },
+                    {"user_id": user["_id"]},
                     app.config["SECRET_KEY"],
-                    algorithm = "HS256"
+                    algorithm="HS256"
                 )
-                return dict(message = "Successfully fetched auth token", data = user), 200
+                return dict(message="Successfully fetched auth token", data=user), 200
             except Exception as e:
-                return dict(message = "Failed to issue an authorization token", error = str(e), data = None), 500
+                return dict(message="Failed to issue an authorization token", error=str(e), data=None), 500
 
         # user == None:
-        return dict(message = "Invalid credentials", data = None, error = "Unauthorized"), 401
+        return dict(message="Invalid credentials", data=None, error="Unauthorized"), 401
 
     except Exception as e:
-        return dict(message = "Something went wrong", error = str(e), data = None), 500
+        return dict(message="Something went wrong", error=str(e), data=None), 500
