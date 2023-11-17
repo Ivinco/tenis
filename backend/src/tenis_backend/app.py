928252f8ca0c851445262c51464b8b3f6a5c24dd
Valery Panov <valery@ivinco.com>
2023-11-17 11:03:31 +0000

    Implement 2-token auth

--- backend/src/tenis_backend/app.py
+++ backend/src/tenis_backend/app.py
@@ -1,81 +1,53 @@
 import os
 import jwt
 import atexit
-from flask import Flask, request
-from flask_cors import CORS
+from datetime import timedelta
 from pymongo import MongoClient
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 from flask_sock import Sock
-from .auth import token_required, sock_auth
+from werkzeug.exceptions import Unauthorized, BadRequest, InternalServerError
+from .auth import token_required, create_token
 from .user import User
 
-# Get environment variables - probably later to be moved to config file
-mongo_host = os.getenv('MONGO_HOST', 'localhost')  # Default to 'localhost' if not set
-mongo_dbname = os.getenv('MONGO_DBNAME', 'tenis')  # Default to 'database' if not set
-connection_string = f"mongodb://{mongo_host}/{mongo_dbname}"
-secret_key = os.getenv('SECRET', 'big tenis')  # Secret key to use
-
+# App configuration parameters
 app = Flask(__name__)
 sock = Sock(app)
 CORS(app)
+mongo_host = os.getenv('MONGO_HOST', 'localhost')  # Default to 'localhost' if not set
+mongo_dbname = os.getenv('MONGO_DBNAME', 'tenis')  # Default to 'database' if not set
+connection_string = f"mongodb://{mongo_host}/{mongo_dbname}"
 app.mongodb_client = MongoClient(connection_string)
 app.db = app.mongodb_client[mongo_dbname]
-app.config['SECRET_KEY'] = secret_key
+app.config['SECRET_KEY'] = os.getenv('SECRET', 'big-tenis')
 
+# This can be useful for testing
+#app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(minutes=1)
+#app.config["JWT_REFRESH_TOKEN_EXPIRES"] = timedelta(minutes=5)
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
+app.config["JWT_REFRESH_TOKEN_EXPIRES"] = timedelta(days=30)
 
 def cleanup_on_shutdown():
     """Properly close MongoDB connection on shutdown"""
     app.mongodb_client.close()
-
-
 atexit.register(cleanup_on_shutdown)
 
 
-@sock.route('/alerts')
-def alerts(ws):
-    if not sock_auth(request):
-        ws.close(reason=1000, message="Auth failed")
-    while True:
-        data = ws.receive()
-        ws.send(data)
-
-
-@sock.route('/new')
-def new(ws):
-    if not sock_auth(request):
-        ws.close(reason=1000, message="Auth failed")
-    while True:
-        data = ws.receive()
-        ws.send(data)
-
-
-@sock.route('/changed')
-def changed(ws):
-    if not sock_auth(request):
-        ws.close(reason=1000, message="Auth failed")
-    while True:
-        data = ws.receive()
-        ws.send(data)
-
-
-@sock.route('/resolved')
-def resolved(ws):
-    if not sock_auth(request):
-        ws.close(reason=1000, message="Auth failed")
-    while True:
-        data = ws.receive()
-        ws.send(data)
-
+#
+# API handles
+#
 
 @app.route("/")
 def index():
     return "Hello, world!\n", 200
 
-
+# Method that just returns user's email
+# We use @token_required with no parameters, this is enough to make sure the user is authenticated
 @app.route("/whoami")
-@token_required
-def whoami(current_user):
+@token_required()
+def whoami(user):
     """Sample method to demonstrate @token_required decorator"""
-    return "You are authorized as %s\n" % (current_user['email']), 200
+    return "You are authorized as %s\n" % (user['email']), 200
 
 
 @app.route("/healz")
@@ -86,31 +58,50 @@ def healz():
         return f"Mongo error: {e!r}\n", 500
     return "tenis is big as ever\n", 200
 
-
+# Authenticate, get two tokens
 @app.route("/auth", methods=["POST"])
 def login():
+    data = request.get_json(force=True, silent=False)
+    if not data:
+        raise BadRequest("Please provide user credentials in JSON format")
+
+    user = User().login(
+        data.get('email'),
+        data.get('password')
+    )
+    if not user:
+        raise Unauthorized("Invalid credentials")
+
     try:
-        data = request.get_json(force=True, silent=False)
-        if not data:
-            return dict(message="Please provide user credentials in JSON format", data=None, error="Bad request"), 400
-
-        user = User().login(
-            data.get('email'),
-            data.get('password')
-        )
-        if user:
-            try:
-                user["token"] = jwt.encode(
-                    {"user_id": user["_id"]},
-                    app.config["SECRET_KEY"],
-                    algorithm="HS256"
-                )
-                return dict(message="Successfully fetched auth token", data=user), 200
-            except Exception as e:
-                return dict(message="Failed to issue an authorization token", error=str(e), data=None), 500
-
-        # user == None:
-        return dict(message="Invalid credentials", data=None, error="Unauthorized"), 401
-
+        access_token = create_token(user["_id"], app.config["SECRET_KEY"], 'access', app.config["JWT_ACCESS_TOKEN_EXPIRES"])
+        refresh_token = create_token(user["_id"], app.config["SECRET_KEY"], 'refresh', app.config["JWT_REFRESH_TOKEN_EXPIRES"])
+        return jsonify(access_token=access_token, refresh_token=refresh_token), 200
     except Exception as e:
-        return dict(message="Something went wrong", error=str(e), data=None), 500
+        raise InternalServerError("Failed to create JWT token")
+
+
+# We are using the `refresh=True` options in jwt_required to only allow
+# refresh tokens to access this route.
+@app.route("/refresh")
+@token_required(refresh=True)
+def refresh(user):
+    try:
+        access_token = create_token(user["_id"], app.config["SECRET_KEY"], 'access', app.config["JWT_ACCESS_TOKEN_EXPIRES"])
+        return jsonify(access_token=access_token)
+    except Exception as e:
+        raise InternalServerError("Failed to issue refresh token")
+
+
+#
+# Websocket channels
+#
+
+@sock.route("/alerts")
+@token_required()
+def alerts(user, ws):
+    """ Just return input back, prefixed with current user name """
+    while True:
+        data = ws.receive()
+        ws.send(user['email'] + ': ' + data)
+
+# more channels can be added with @sock.route
