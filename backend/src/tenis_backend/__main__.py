fb8bebd3eea07a071e3a95ccbf65c41e84f0f0fd
Valery Panov <valery@ivinco.com>
2023-11-24 19:08:54 +0000

    Fix cors init for socketio

--- backend/src/tenis_backend/__main__.py
+++ backend/src/tenis_backend/__main__.py
@@ -4,4 +4,4 @@ from .app import app, socketio
 port = app.config['LISTEN_PORT']
 host = app.config['LISTEN_HOST']
 if __name__ == '__main__':
-    socketio.run(app, host=host, port=int(port), cors_allowed_origins="http://*.ivinco.com")
+    socketio.run(app, host=host, port=int(port))
