5faa9b654d2cfc5fc0c7d7c471d062586af13dcb
Mykhailo Liepieshko <mykhailo@ivinco.com>
2024-03-07 11:00:28 +0200

    one more typo fix

--- input_plugins/file/tenis_input_file.py
+++ input_plugins/file/tenis_input_file.py
@@ -9,7 +9,7 @@ schema = {
             "type": "object",
             "additionalProperties": {
                 "type": "string"
-            },
+            }
         }
         "alert_definition": {
             "properties": {
