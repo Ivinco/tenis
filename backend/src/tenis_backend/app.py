d5610e64d95a5528027002284947e0e18e1a9452
Станислав Левченко <lev-stas@yandex.ru>
2024-05-14 16:29:30 +0300

    TT-197 added alert_id to response

--- backend/src/tenis_backend/app.py
+++ backend/src/tenis_backend/app.py
@@ -172,8 +172,9 @@ def get_history_alerts(start_datetime, end_datetime):
         alert_id = record['alert_id']
         record['logged'] = record['logged'].timestamp()
         if alert_id not in alerts_by_id or record['logged'] > alerts_by_id[alert_id]['logged']:
-            filtered_record = {key: value for key, value in record.items() if key not in ['_id', 'alert_id']}
+            filtered_record = {key: value for key, value in record.items() if key not in ['_id']}
             filtered_record['logged'] = int(record['logged'])
+            filtered_record['alert_id'] = str(record['alert_id'])
             alerts_by_id[alert_id] = filtered_record
 
     result = list(alerts_by_id.values())
