c9c7f2a5337d2e354c01eb779c39d5e9a2a52b93
Станислав Левченко <lev-stas@yandex.ru>
2024-07-15 15:56:00 +0300

    TT-228 [front] Make method to save alert comments on UI side.

--- frontend/src/components/AlertsDetails/AlertsDetails.jsx
+++ frontend/src/components/AlertsDetails/AlertsDetails.jsx
@@ -13,13 +13,31 @@ import {setModalError} from "../../store/reducers/modalReducer";
 const AlertsDetails = ({details, history}) => {
     const dispatch = useDispatch();
     const [commentFormIsOpened, setCommentFormIsOpened] = useState(false)
-    const [commentFormContent, setCommentFormContent] = useState('')
+    const [commentFormContent, setCommentFormContent] = useState(details.comment)
     const user = useSelector( state => state.authReducer.user)
     const textareaRef = useRef(null)
     const [historyStart, setHistoryStart] = useState(new Date(Date.now() - 24 * 60 * 60 * 1000))
     const [historyEnd, setHistoryEnd] = useState(new Date(Date.now()))
     const [data, setData] = useState([])
     const [colors, setColors] = useState([])
+    const [processedString, setProcessedString] = useState('')
+    const [alertComment, setAlertComment] = useState(null)
+
+    useEffect(() => {
+        const process = details.comment.split(/\s+/).map(word => processAlertComment(word, user.usersCommentReplaceRules))
+        setProcessedString(process)
+    },[details.comment])
+
+    useEffect(() => {
+        const comment = (
+            <>
+                {processedString ? processedString.map((element, index) => (
+                    <React.Fragment key={index}>{element} </React.Fragment>
+                )) : <></>}
+            </>
+        )
+        setAlertComment(comment)
+    },[processedString])
 
     //init data array
     const dataSample = [
@@ -86,7 +104,6 @@ const AlertsDetails = ({details, history}) => {
         setCommentFormIsOpened(false)
         document.getElementById('commentArea').value = ''
         const alert_id = details.alert_id ? details.alert_id : details._id
-        const processedString = commentFormContent.split(/\s+/).map(word => processAlertComment(word, user.usersCommentReplaceRules))
         const commentRequest = {
             alert_id: alert_id,
             comment: commentFormContent
@@ -96,15 +113,8 @@ const AlertsDetails = ({details, history}) => {
         } catch (e) {
             dispatch (setModalError("Oops, something went wrong"))
         }
-
+        details.comment = commentFormContent
         setCommentFormContent('')
-        details.comment = (
-            <>
-                {processedString.map((element, index) => (
-                    <React.Fragment key={index}>{element} </React.Fragment>
-                ))}
-            </>
-        )
     }
 
     const handleEnterKey = (e) => {
@@ -204,7 +214,7 @@ const AlertsDetails = ({details, history}) => {
                             e.preventDefault()
                             onCommentClick()
                         }}/>
-                        <p className={styles.alertInfoValue}>{details.comment}</p>
+                        <p className={styles.alertInfoValue}>{alertComment}</p>
                     </li>
                 </ul>
                 <div className={commentFormIsOpened ? styles.commentBlock : styles.disabledBlock}>
@@ -213,6 +223,7 @@ const AlertsDetails = ({details, history}) => {
                               maxLength="100"
                               id="commentArea"
                               ref={textareaRef}
+                              value={commentFormContent}
                               onChange={(e) => setCommentFormContent(e.target.value)}
                               onKeyDown={handleEnterKey}
                     />
