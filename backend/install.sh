d3f7dce34a2df50357aec8453c0c396598afa58d
Mykhailo Liepieshko <mykhailo@ivinco.com>
2024-08-12 09:28:01 +0200

    TT-171 [backend] DB migrations support

--- backend/install.sh
+++ backend/install.sh
@@ -3,10 +3,10 @@
 set -e
 cd $(dirname "$0")
 
-sudo python3 -m pip install --upgrade pip
+sudo python3 -m pip install --upgrade pip --break-system-packages
 dir=$(pwd)
 package=$(ls -t dist/tenis_backend-*.whl | head -1) # latest file in terms of mtime
-sudo pip3 install --upgrade "file://$dir/$package" --force-reinstall
-sudo install -oroot -groot -m644 tenis-backend.service /lib/systemd/system/tenis-backend.service
-sudo systemctl daemon-reload
-echo "Run \`sudo systemctl restart tenis-backend.service' to start Tenis backend server"
+sudo pip3 install --upgrade "file://$dir/$package" --force-reinstall --break-system-packages
+#sudo install -oroot -groot -m644 tenis-backend.service /lib/systemd/system/tenis-backend.service
+#sudo systemctl daemon-reload
+#echo "Run \`sudo systemctl restart tenis-backend.service' to start Tenis backend server"
--- backend/src/tenis_backend/__main__.py
+++ backend/src/tenis_backend/__main__.py
@@ -1,6 +1,7 @@
 from os import getenv
 from sys import stderr, exit
 from .app import app, socketio
+from .mongo_migrate import run_migrations
 
 port = app.config['LISTEN_PORT']
 host = app.config['LISTEN_HOST']
@@ -12,5 +13,9 @@ if not app.config['API_TOKEN']:
 # use_reloader=False is needed because of
 # https://stackoverflow.com/questions/9449101/how-to-stop-flask-from-initialising-twice-in-debug-mode/9476701#9476701
 if __name__ == '__main__':
+    
+    print("Updating MongoDB...")
+    run_migrations()
+
     print("Starting the server...")
     socketio.run(app, host=host, port=int(port), allow_unsafe_werkzeug=True, use_reloader=False)
